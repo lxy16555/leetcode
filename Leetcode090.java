78643070f275776a3bc0474845c7f12bf3ab140a
Lyle <lxy16555@gmail.com>
2015-10-29 23:27:42 -0400

    subsets with duplicates

---
+++ Leetcode090.java
@@ -0,0 +1,30 @@
+package test;
+
+//找有重复数字的subset，做法跟找没重复subset一样，加一步比较是否已含有
+public class Leetcode090 {
+    List<Integer> temp = new ArrayList<>();
+    
+    public void addSubsets(int[] nums, int start, List<List<Integer>> ans) {
+        if (start > nums.length)
+            return;
+        if (ans.contains(temp))
+            return;
+            
+        ans.add(new ArrayList<>(temp));
+        
+        for (int i=start; i<nums.length; i++) {
+            temp.add(nums[i]);
+            addSubsets(nums, i+1, ans);
+            temp.remove(temp.size()-1);
+        }
+    }
+    
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        addSubsets(nums, 0, ans);
+        
+        return ans;
+    }
+}
